1acac18ad3e13b8d70f08eaca5a4bca14feaf6d0
Corey Bradshaw <cjabradshaw@gmail.com>
2020-11-07 00:08:51 +1030

    Update get.profile.func.R

--- get.profile.func.R
+++ get.profile.func.R
@@ -20,14 +20,14 @@ library(scholar)
 ## load function
 get.profiledat.func <- function(ids.vector, genders.vector) { # 'ids.vector' is the character vector of Google Scholar IDs, and 'genders.vector' is the character vector of researcher gender
   profiles.list <- lapply(ids.vector, get_profile)
-  hs <- i10s <- maxcits <- Y1s <- rep(NA,length(ids))
-  for (r in 1:length(ids)) {
+  hs <- i10s <- maxcits <- Y1s <- rep(NA,length(ids.vector))
+  for (r in 1:length(ids.vector)) {
     hs[r] <- profiles.list[[r]]$h_index
     i10s[r] <- profiles.list[[r]]$i10_index
-    maxcits[r] <- get_publications(profiles.list[[r]])[1,5]
-    Y1s[r] <- get_oldest_article(profiles.list[[r]])
+    maxcits[r] <- get_publications(as.character(profileslist[[r]]$id))$cites[1]
+    Y1s[r] <- get_oldest_article(profileslist[[r]]$id)
   }
-  input.data <- data.frame(ids, genders.vector, i10s, hs, maxcits, Y1s)
+  input.data <- data.frame(ids.vector, genders.vector, i10s, hs, maxcits, Y1s)
   colnames(input.data) <- c("personID", "gender", "i10", "h", "maxcit","firstyrpub")
   return(input.data)
 }
